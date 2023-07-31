f2929b1eb8ec892970a31ad29489cf20af005983
Jagadeesh Kumar Reddy Atla <98443978+jagadeesh-atla@users.noreply.github.com>
2023-07-31 16:58:59 +0530

    added search button

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import { courses as items } from "../data";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "../styles/index.module.css";
 
 export default function Home() {
   const router = useRouter();
+  const [search, setSearch] = useState("");
 
+  const handeleOnSearch = (string, result) => {
+    console.log(string);
+    setSearch(string);
+  };
+  
   const handleOnSelect = (item) => {
     const code = item.name.slice(0, 6).trim();
     router.push(`/${code}`);
@@ -29,13 +35,21 @@ export default function Home() {
         <h1 className={styles.h1}>Lecture Plans</h1>
       </div>
       <div style={{ width: "400px" }}>
+        <div>
+      <button  style={{marginLeft: "45%", marginBottom: "2%"}} onClick={()=>{
+          const code = search.toUpperCase();
+          router.push(`/${code}`);
+        }}>Search</button>
+        </div>
         <ReactSearchAutocomplete
           items={items}
+          onSearch={handeleOnSearch}
           onSelect={handleOnSelect}
           autoFocus
           formatResult={formatResult}
           placeholder="Course Code or Name"
         />
+        
       </div>
     </div>
   );
